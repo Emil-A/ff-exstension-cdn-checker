72eaa5bcf68f72a05284534b6b5ca7df8c7b8e40
Emil Aite <emil.a.aite@hrsdc-rhdcc.gc.ca>
2016-03-16 10:15:51 -0400

    adjustments to layout done

--- popups/index.js
+++ popups/index.js
@@ -1,5 +1,5 @@
 //Chrome documentation https://developer.chrome.com/extensions/optionsV2
-//Should be supported by firefox soon https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/runtime/openOptionsPage
+//Should be supported by firefox soon https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/runtime
 function openOptions(form) {
 	if (browser.runtime.openOptionsPage) {
 		// New way to open options pages, if supported (Chrome 42+).
@@ -10,4 +10,16 @@ function openOptions(form) {
 	}
 }
 
+function close() {
+	alert("close");
+	window.close();
+}
+
+function doSomething() {
+	//run some function or call a content_script action
+	alert("do something");
+}
+
 document.getElementById('options').addEventListener('click', openOptions);
+document.getElementById('okay').addEventListener('click', close);
+document.getElementById('submit').addEventListener('click', doSomething);
